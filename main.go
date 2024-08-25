d3510f5222b99f8a39ba15ee5b6b0e7da0f084da
Toni Pokki <tpokki@users.noreply.github.com>
2024-08-25 12:50:48 +0300

    feat(logging): log requests and status

--- main.go
+++ main.go
@@ -6,7 +6,9 @@ import (
 	"flag"
 	"fmt"
 	"io"
+	"log"
 	"net/http"
+	"os"
 )
 
 type ForwardHandler struct {
@@ -18,6 +20,7 @@ var port int
 var target string
 var certificate string
 var privateKey string
+var verbose bool
 
 // init binds flags for command line arguments
 func init() {
@@ -25,6 +28,7 @@ func init() {
 	flag.StringVar(&target, "target", "", "target host")
 	flag.StringVar(&certificate, "certificate", "certificate.crt", "certificate file")
 	flag.StringVar(&privateKey, "key", "private.key", "key file")
+	flag.BoolVar(&verbose, "verbose", true, "verbose output")
 }
 
 // main creates local http server, and forwards all requests to the target server
@@ -33,6 +37,22 @@ func main() {
 	// Parse command line arguments
 	flag.Parse()
 
+	// Check if target host is provided
+	if target == "" {
+		fmt.Printf("Target host is required\n\n")
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	// set output verbosity
+	if verbose {
+		log.SetFlags(log.LstdFlags)
+	} else {
+		log.SetFlags(0)
+		log.SetOutput(io.Discard)
+	}
+
+	log.Printf("🟢 Starting local forwarder to %s...\n", target)
 	err := http.ListenAndServe(fmt.Sprintf(":%d", port), ForwardHandler{client: createClient()})
 	fmt.Printf("exit: %+v\n", err)
 }
@@ -47,8 +67,12 @@ func (f ForwardHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	resp, err := f.client.Do(fr)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
+		log.Printf("🔴 %s %s\n", fr.Method, fr.URL.Path)
+		log.Printf("🔴 %s\n", err.Error())
 		return
 	}
+
+	log.Printf("🟢 %s %s [%d]\n", fr.Method, fr.URL.Path, resp.StatusCode)
 
 	// Copy response from target server to local client
 	for k, v := range resp.Header {
@@ -73,7 +97,8 @@ func createClient() *http.Client {
 func loadCertificate(certificate, privateKey string) tls.Certificate {
 	cert, err := tls.LoadX509KeyPair(certificate, privateKey)
 	if err != nil {
-		panic(err)
+		fmt.Printf("%+v\n", err.Error())
+		os.Exit(1)
 	}
 	return cert
 }
